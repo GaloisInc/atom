5335f6e3a31f368d691b07715aa33621091dea53
Benjamin Jones <bjones@galois.com>
2016-10-19 16:24:39 -0700

    Fiddle with build scripts

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+FLAGS=""
+if [[ "${1:-}" == "develop" ]]; then
+    FLAGS="-f develop"
+fi
+
 cabal sandbox init
 cabal install --only-dependencies --enable-tests
+cabal configure $FLAGS
 cabal build
 cabal test
